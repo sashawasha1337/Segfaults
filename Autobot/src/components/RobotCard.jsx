0e3d4c921c663d983601b64930704ef858a3d300
0x53544152 <dmanifacio@csus.edu>
2025-03-29 21:21:08 -0700

    Update RobotCard.jsx

--- Autobot/src/components/RobotCard.jsx
+++ Autobot/src/components/RobotCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import "../styles/robotcard.css";
+import "../styles/RobotCard.css";
 
 export const RobotCard = ({ imgSrc, imgAlt, title, description, buttonText, link }) => {
     console.log('RobotCard component is rendering');
