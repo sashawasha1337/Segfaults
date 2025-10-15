71d2770f2f18fc1b756f7c0e5b5a09517c68cae4
sashawasha1337 <sashamarmorstein@gmail.com>
2025-10-14 21:31:59 -0700

    added notes to norosmotorscript

--- RobotScripts/norosmotorscript.py
+++ RobotScripts/norosmotorscript.py
@@ -2,8 +2,11 @@
 #the exact pins will depend on the motor driver board and wiring used
 ##this is not tested as I only had access to paralax controller with proprietary software 
 ##but this is the general idea
-#assumptions: l293d motor driver board 
-##configured using this example for first motor:
+
+#assumes l293d motor driver board and rasperry pi
+
+#code derived from and motor driver board
+#configured using this example for first motor:
 ## https://www.electronicshub.org/controlling-a-dc-motor-with-raspberry-pi/
 #^easiest way to understand expected wiring of motor driver board
 
@@ -22,8 +25,8 @@ Motor1E = 25##pin1 on motor driver/physical pin 22
 Motor2A=17#pin 10
 Motor2B=27#pin 15
 Motor2E=22#pin 9 on motor driver
-
- 
+# motor driver pins do translate to specific electrical signals when powered in specific ports
+# # we need to understand our chip and power it accordingly using simple gpio output instructions
 def setup():
 	GPIO.setmode(GPIO.BCM)				# GPIO Numbering
 	GPIO.setup(Motor1A,GPIO.OUT)  # All pins as Outputs
