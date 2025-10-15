4594f5bc59ae1525087cbd0cb79e58bb873e9940
0x53544152 <dmanifacio@csus.edu>
2025-10-14 17:33:59 -0700

    SCRUM-175: Add unit tests and integration tests for nodes SCRUM-176: Implement a diagnostics node for monitoring node health/status

--- RobotScripts/src/navigation/setup.py
+++ RobotScripts/src/navigation/setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'pyserial', 'pynmea2', 'firebase-admin'],
     zip_safe=True,
     maintainer='patrick',
     maintainer_email='patrick.doolittle@gmail.com',
@@ -21,7 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
             'navigation_node = navigation.NavigationNode:main',
-            'gps_node = navigation.gpsNode:main',
+            'gps_node = navigation.gpsNode:run',
             'obstacle_avoidance = navigation.obstacle_avoidance:main',
             'geofence_node = navigation.geofenceControllerNode:main',
         ],
