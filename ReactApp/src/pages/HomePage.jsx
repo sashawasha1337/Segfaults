ee1c4e4fa14a50dfb178bbfc8e4d7dbf29e6684c
Manpreet Rai <manpreetrai9000@gmail.com>
2025-10-27 12:12:04 -0700

    #193 limiting robots and adding paging on homepage

--- ReactApp/src/pages/HomePage.jsx
+++ ReactApp/src/pages/HomePage.jsx
@@ -72,83 +72,162 @@ function HomePage() {
   };
 
   const [robots, setRobots] = useState([]);
-  const { currentUser } = useAuth(); 
+  const { currentUser } = useAuth();
+  const [allRobotIds, setAllRobotIds] = useState([]);
+  const [page, setPage] = useState(1);
+  const pageSize = 6;
+
+  const loadPage = async (robotIds, pageNum) => {
+    const startIndex = (pageNum - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+    const pageIds = robotIds.slice(startIndex, endIndex);
+
+    const robotsData = [];
+    for (const id of pageIds) {
+      const robotRef = doc(db, "robots", id);
+      const robotSnap = await getDoc(robotRef);
+      if (robotSnap.exists()) {
+        robotsData.push({ id: robotSnap.id, ...robotSnap.data() });
+      }
+    }
+
+    setRobots(robotsData);
+    setPage(pageNum);
+  };
+
   useEffect(() => {
-    if(!currentUser){
+    if (!currentUser) {
       console.error("No current user found.");
       return;
     }
-    const fetchRobots = async() => {
-      try{
+
+    const fetchProfileAndRobots = async () => {
+      try {
         const profilesRef = doc(db, "profiles", norm(currentUser.email));
         const profileSnap = await getDoc(profilesRef);
 
-        console.log("Current user ID:", currentUser.uid);
-
-        const robotsData = [];
-        if(profileSnap.exists()){
-          const profileData = profileSnap.data();
-          const robotIds = profileData.robots || [];
-
-          for (const ids of robotIds){
-            const robotsRef = doc(db, "robots", ids);
-            const robotSnap = await getDoc(robotsRef);
-            if(robotSnap.exists()){
-              robotsData.push({id: robotSnap.id, ...robotSnap.data()});
-            }
-          }
+        if (!profileSnap.exists()) {
+          console.error("Profile not found for user:", currentUser.email);
+          return;
         }
-        setRobots(robotsData);
-      }catch (error) {
+
+        const profileData = profileSnap.data();
+        const robotIds = profileData.robots || [];
+        setAllRobotIds(robotIds);
+
+        // Automatically load the first page
+        loadPage(robotIds, 1);
+      } catch (error) {
         console.error("Error fetching robots:", error);
       }
     };
-    fetchRobots();
+
+    fetchProfileAndRobots();
   }, [currentUser]);
 
 
   return (
-    <div className="homepage">
-      <SettingsButton path="/UserSettingsPage" /> 
-      <AddButton path="/AddRobotPage" />
-      
-      <Button variant="contained"
-        onClick={() => navigate("/ActivityLogPage")}
+    <>
+      <Box className="homepage"
         sx={{
-          position: "absolute",
-          top: 100,
-          right: 35,
-          textTransform: "none",
-          borderRadius: "10px",
-          width: "125px",
-          height: "50px",
-          fontSize: "1.0rem",
-          backgroundColor: "black",
+          width: "100%", 
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "row",
         }}
       >
-        Activity Log
-      </Button>
+            {/* --- Top buttons --- */}
+      <Box>
+        <SettingsButton path="/UserSettingsPage" />
+        <AddButton path="/AddRobotPage" />
 
-      {robots.length === 0 ? (
-        <p style={{ marginTop: "200px", textAlign: "center" }}>
-          No robots found. Click the + button to add one!
-        </p>
-      ) : (
-        robots.map((robot, index) => (
-          <RobotCard
-            key={robot.id}
-            imgSrc="https://images.squarespace-cdn.com/content/v1/5a3c1a29f9a61e2987882112/bee5c58a-5b2c-4302-bb18-433dd7bd5f2c/ROSmaster.jpeg"
-            imgAlt={`Robot ${robot.id}`}
-            title={robot.name || `Robot ${index + 1}`}
-            description={`IP: ${robot.ipAddress}`}
-            admin={robot.admin}
-            buttonText="FPV/Control"
-            link={`/RobotDashboardPage/${robot.id}`}
-            onDelete={() => handleDeleteRobot(robot.id)}
-          />
-        ))
-      )}
-    </div>
+        <Button
+          variant="contained"
+          onClick={() => navigate("/ActivityLogPage")}
+          sx={{
+            position: "absolute",
+            top: 100,
+            right: 35,
+            textTransform: "none",
+            borderRadius: "10px",
+            width: "125px",
+            height: "50px",
+            fontSize: "1rem",
+            backgroundColor: "black",
+          }}
+        >
+          Activity Log
+        </Button>
+      </Box>
+          {/* --- Robot list --- */}
+        <div> {/* div need around box or else previous and next buttons are stretched on page with 1 robot */}
+          <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: 3,           // spacing between cards
+              justifyContent: "center",
+              paddingTop: 4,
+              paddingX: 2,
+            }}
+          >
+            {robots.length === 0 ? (
+              <p style={{ marginTop: "200px", textAlign: "center" }}>
+                No robots found. Click the + button to add one!
+              </p>
+            ) : (
+              robots.map((robot, index) => (
+                <RobotCard
+                  key={robot.id}
+                  imgSrc="https://images.squarespace-cdn.com/content/v1/5a3c1a29f9a61e2987882112/bee5c58a-5b2c-4302-bb18-433dd7bd5f2c/ROSmaster.jpeg"
+                  imgAlt={`Robot ${robot.id}`}
+                  title={robot.name || `Robot ${index + 1}`}
+                  description={`IP: ${robot.ipAddress}`}
+                  admin={robot.admin}
+                  buttonText="FPV/Control"
+                  link={`/RobotDashboardPage/${robot.id}`}
+                  onDelete={() => handleDeleteRobot(robot.id)}
+                />
+              ))
+            )}
+          </Box>
+          {allRobotIds.length > pageSize && (
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: 6,
+                marginBottom: 6,
+                width: "100%",
+              }}
+            >
+              <Button
+                variant="outlined"
+                disabled={page === 1}
+                onClick={() => loadPage(allRobotIds, page - 1)}
+                sx={{
+                  marginRight: 2 ,
+                  minWidth: 120,
+                }}
+              >
+                Previous
+              </Button>
+              <Button
+                variant="outlined"
+                disabled={page * pageSize >= allRobotIds.length}
+                onClick={() => loadPage(allRobotIds, page + 1)}
+                sx={{
+                  minWidth: 120,
+                }}
+              >
+                Next
+              </Button>
+            </Box>
+          )}
+        </div>
+      </Box>
+      
+    </>
   );
 }
 
